ebb727c4b2326e2cf0410b14510cf3eaddb9d3cd
hvkjscrbvw <aurav.deep.bawari@gmail.com>
2025-07-25 11:21:26 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+year = input('year?')
+year = int(year)
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print('yes')
+        else: 
+            print('no')
+    else:
+        print('yes')
+else:
+     print('no')
